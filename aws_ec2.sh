49075cc0b31cb29d127db73b59eb508ce0968048
Thomas Weigel <thomas.weigel@chantofwaves.com>
2021-11-11 16:42:26 +0000

    Added some features to aws_ec2.sh

--- aws_ec2.sh
+++ aws_ec2.sh
@@ -1,83 +1,133 @@
 #!/usr/bin/env bash
 
 aws.ec2() {
-    local -r command="${1:-}"
+  local -r command="${1:-}"
+  shift;
 
-    case $command in
-        ls)
-            local region="${2:-$(aws.region)}"
-
-            aws ec2 describe-instances \
-              --region $region \
-              --query "Reservations[].Instances[?State.Name=='running'].Tags[][?Key=='Name'].Value" \
-              --output text
-            ;;
-        vpcs)
-            local region="${2:-$(aws.region)}"
-            aws ec2 describe-instances \
-              --region $region \
-              --query 'Reservations[].Instances[].{Instance:InstanceId,Instance:VpcId,Name:Tags[?Key==`Name`]|[0].Value}' \
-              --output text \
-              | sort -u | column -t
-            ;;
-        ssm)
-            local name="${2:-}"
-            local region="${3:-$(aws.region)}"
-            aws.ssm $name $region
-            ;;
-        *)
-            echo 'aws.ec2 ls            lists names of ec2 instances in current region'
-            echo 'aws.ec2 ls $region    as above, for given region'
-            echo 'aws.ec2 vpcs          lists names of VPCs in current region'
-            echo 'aws.ec2 vpcs $region  as above, for given region'
-            echo 'aws.ec2 ssm $name     attempts to connect via SSM to named ec2 instance'
-            echo 'aws.ec2 ssm $name $region   as above, for given region'
-            ;;
-    esac
+  case $command in
+    ls)
+      __ec2_names "${1:-}"
+      ;;
+    id)
+      __ec2_id "$@"
+      ;;
+    vpcs)
+      __ec2_vpcs "$@"
+      ;;
+    ssm)
+      __ssm_bash "$@"
+      ;;
+    update-agent)
+      __ssm_update_agent "$@"
+      ;;
+    port)
+      __ssm_port_forward "$@"
+      ;;
+    proxy)
+      __ssm_proxy "$@"
+      ;;
+    *)
+      echo 'aws.ec2 COMMAND ARGUMENTS'
+      echo '[$region] is always an optional last argument: defaults to current region'
+      echo ''
+      echo 'ls                    lists names of EC2 instances in region'
+      echo 'id $name              acquire instance ID for EC2 instance by name'
+      echo 'vpcs                  table of "VPC -> EC2 instances" for region'
+      echo 'ssm $name             connect via SSM to named EC2 instance'
+      echo 'update-agent $name    updates the SSM Agent on named EC2 instance'
+      echo 'port $name $port      sets up port-forwarding to EC2 instance'
+      echo 'proxy $name           sets up an SSH primitive to EC2 instance'
+      echo '                      (useful for some opinionated programs)'
+      ;;
+  esac
 }
 
 
-aws.ls-ec2-names() {  # lists available Name tags in EC2 for current AWS profile
-    aws ec2 describe-instances \
-      --query "Reservations[].Instances[?State.Name == 'running'].Tags[][?Key == 'Name'].Value" \
-      --output text
+__ec2_vpcs(){
+  local region="${2:-$(aws.region)}"
+  aws ec2 describe-instances \
+    --region $region \
+    --query 'Reservations[].Instances[].{Instance:InstanceId,Instance:VpcId,Name:Tags[?Key==`Name`]|[0].Value}' \
+    --output text \
+    | sort -u | column -t
 }
 
 
-aws.ec2-by-name() {  # finds an EC2 instance by its Name tag
-    local name="${1:-}"
-    if [[ -z $name || $name == "help" ]]; then
-        echo "Usage: aws.ec2-by-name INSTANCE_TAG_NAME"
-        (exit 1)
-    fi
-
-    aws ec2 describe-instances \
-      --filter "Name=tag:Name,Values=${name}" \
-      --query "Reservations[].Instances[?State.Name == 'running'].InstanceId[]" \
-      --output text
+__ec2_names() {  # get list of Name tags for active EC2 instances
+  local -r region="${1:-$(aws.region)}"
+  aws ec2 describe-instances \
+    --region "$region" \
+    --query "Reservations[].Instances[?State.Name=='running'].Tags[][?Key=='Name'].Value" \
+    --output text
 }
 
 
-aws.ssm() {  # connect to an EC2 instance via SSM by its Name tag
-    local name="${1:-}"
-    local region="${2:-$(aws.region)}"
-
-    local instance="$(
-        aws ec2 describe-instances \
-          --output text \
-          --region $region \
-          --filter "Name=tag:Name,Values=${name}" \
-          --query "Reservations[].Instances[?State.Name == 'running'].InstanceId[]"
-    )" || (exit 1)
-
-    local instance="$(aws.ec2-by-name $name)" || (exit 1)
-    if [[ -z $instance ]]; then
-        echo "No instance available by the name ${name}."
-        (exit 1)
-    fi
-
-    aws ssm start-session \
-      --target $instance \
-      --document-name AWS-StartInteractiveCommand \
-      --parameters 'command="bash"'
+__ec2_id() {  # get Instance ID from Name tag
+  local -r name="${1:-}"
+  local -r region="${2:-$(aws.region)}"
+  aws ec2 describe-instances \
+    --filter "Name=tag:Name,Values=${name}" \
+    --query "Reservations[].Instances[?State.Name=='running'].InstanceId[]" \
+    --output text
 }
+
+
+__ssm_bash() {  # open a bash session on the EC2 instance
+  local -r name="${1}"
+  local -r region="${2:-$(aws.region)}"
+  local -r instance="$(__ec2_id $name)" || (exit 1)
+
+  if [[ -z $instance ]]; then
+    echo "No instance available by name '${name}'."
+    (exit 1)
+  fi
+
+  aws ssm start-session \
+    --target "$instance" \
+    --document-name AWS-StartInteractiveCommand \
+    --parameters 'command="bash"'
+}
+
+
+__ssm_port_forward() {  # opens a port-forwarding session
+  local -r name="${1}"
+  local -r port="${2}"
+  local -r region="${3:-$(aws.region)}"
+  local -r instance="$(__ec2_id $name)"
+
+  aws ssm start-session \
+    --target "$instance" \
+    --document-name AWS-StartPortForwardingSession \
+    --parameters '{"portNumber":["'${port}'"],"localPortNumber":["'${port}'"]}'
+}
+
+
+__ssm_proxy() {
+  local -r name="${1}"
+  local -r region="${2:-$(aws.region)}"
+  local -r instance="$(__ec2_id $name)"
+  aws ssm start-session \
+    --target $instance \
+    --document-name AWS-StartSSHSession \
+    --parameters 'portNumber=22'
+}
+
+
+__ssm_update_agent() {  # update the SSM Agent on the EC2 instance
+  local -r name="${1:-}"
+  local -r region="${2:-$(aws.region)}"
+  local -r instance="$(__ec2_id $name)"
+  aws ssm send-command \
+    --document-name "AWS-UpdateSSMAgent" \
+    --document-version "1" \
+    --targets '[{"Key":"InstanceIds","Values":["'$instance'"]}]' \
+    --parameters '{"version":[""],"allowDowngrade":["false"]}' \
+    --timeout-seconds 600 \
+    --max-concurrency "50" \
+    --max-errors "0"
+}
+
+
+aws.ls-ec2-names() { __ec2_names; } # LEGACY
+aws.ec2-by-name() { __ec2_id "${1:-}"; } # LEGACY
+aws.ssm() { __ssm_bash "$@"; } # LEGACY
